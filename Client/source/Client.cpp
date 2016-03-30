3c2713959998d42d18cd8c343fd6262923923eb1
gauravp94 <gparida94@gmail.com>
2016-03-31 04:15:09 +0530

    makshift client ignore

---
+++ Client/source/Client.cpp
@@ -0,0 +1,26 @@
+#include <rpc>
+
+int main()
+{
+	while(1)
+	{
+		cin>>inp;
+		if(inp.compare("get")==0)
+		{
+
+		}
+		else if(inp.compare("list")==0)
+		{
+
+		}
+		else if(inp.compare("put")==0)
+		{
+
+		}
+		else
+		{
+			cout<<"Wrong Command!"<<endl;
+		}
+	}
+	return 0;
+}
